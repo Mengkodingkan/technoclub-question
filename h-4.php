12fe6ca496fdf94ceb5764c7412db356ec99e5d8
Marli Sumarli <67571006+marlisumarli@users.noreply.github.com>
2023-07-24 16:09:30 +0000

    Soal 8

---
+++ h-4.php
@@ -0,0 +1,9 @@
+<?php
+////////////////////////////////////// Soal 8 //////////////////////////////////////
+//                Gunakan perulangan untuk menampilkan contoh berikut             //
+// contoh :                                                                       //
+//          Technoclub pertemuan 1                                                //
+//          Technoclub pertemuan 2                                                //      
+//          Technoclub pertemuan 3                                                //      
+//          Technoclub pertemuan 4 (sekarang)                                     //
+////////////////////////////////////////////////////////////////////////////////////
