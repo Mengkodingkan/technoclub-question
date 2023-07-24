8ac37b73901364028eecbdc2f85dd41440c78bb2
Marli Sumarli <67571006+marlisumarli@users.noreply.github.com>
2023-07-24 15:28:21 +0000

    soal 8

---
+++ hiburan.php
@@ -0,0 +1,11 @@
+<?php
+////////////////////////////////////// Soal 7 //////////////////////////////////////
+//                     bagaimana agar code ini lebih efisien?                     //
+////////////////////////////////////////////////////////////////////////////////////
+
+echo "Technoclub" . PHP_EOL;
+
+echo "Technoclub pertemuan 1" . PHP_EOL;
+echo "Technoclub pertemuan 2" . PHP_EOL;
+echo "Technoclub pertemuan 3" . PHP_EOL;
+echo "Technoclub pertemuan 4 (Sekarang)" . PHP_EOL;
