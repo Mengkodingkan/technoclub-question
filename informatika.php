17e642466c36ecc7575e70743695483f7de922e9
Marli Sumarli <67571006+marlisumarli@users.noreply.github.com>
2023-07-24 16:09:47 +0000

    Soal 9

---
+++ informatika.php
@@ -0,0 +1,17 @@
+<?php
+////////////////////////////////////// Soal 9 //////////////////////////////////////
+//        Ubah code di bawaha menjadi array object dengan data mahasiswa          //
+////////////////////////////////////////////////////////////////////////////////////
+
+echo "Data mahasiswa";
+$nama = "nama sendiri";
+$prodi = "informatika";
+$kelas = "kelas";
+
+$nama2 = "nama kerabat";
+$prodi2 = "informatika";
+$kelas2 = "kelas";
+
+$nama3 = "nama kerabat";
+$prodi3 = "informatika";
+$kelas3 = "kelas";
